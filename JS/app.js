923d13c58817b110fd4a6973853bf054256b63de
ahrenswett <ahrenswett@gmail.com>
2019-06-05 23:24:57 -0700

    made a plan

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,65 @@
-// selects three random photos
-// displays them side by side
-// each image registers clicks, track the clicks return percentage
-// upon a click three new images should appear
-// 
+'use strict';
+
+
+
+/* 
+selects three random photos
+displays them side by side
+each image registers clicks, track the clicks return percentage
+upon a click three new images should appear
+
+construct an array of picture objects
+    function that loops over contents of a given folder
+    each file is pushed through the constructor
+        constructor that holds
+            name
+            timesClicked
+            timesDisplayed
+            percentClicked
+            percentDisplayed
+
+choose three pics at random form pics array
+    function that limits math. random to length of chosen array
+    for loop that iteragtes through math.random 3 times
+    each time pushing the corresponding oject to currentPics array.
+    timesDisplayed is incremented for each object in array
+        Pics are then pushed to DOM
+
+When a picture is clicked
+    timesClicked propety for that object increments
+    the whole array is pushed to a new array called lastPics
+
+function to check to see what pics lastPics contains
+function to genetrate new currentPics exculding any pics in lastPics
+after 25 iterations event listener is turned off
+function that calculates pecentage clicked and percentage displayed is called
+
+    ****** timesClicked/timesDisplayed = pecentClicked
+    ****** timedDSisplayed/25 = percentDisplayed
+
+
+*/
+
+function Pic(fileName){//requires string
+  this.name = fileName;
+  this.filepath = `./image/${name}.jpg`;
+  this.timesClicked;
+  this.timesDisplayed;
+  this.percentClicked;
+  this.percentDisplayed;
+  pics.push(this);
+}
+
+var pics= [];
+
+var div=document.createElement('div');
+div.setAttribute('id', 'picDiv' );
+
+var img = document.createElement('img');
+img.setAttribute('id', 'pic1');
+img.setAttribute('src', '');
+img.setAttribute('alt', '');
+
+
+new Pic('bag');
+console.log(pics);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Busmall
+
+Author: Ahren Swett 
+Date: June 2019
+
+Busmall is looking for a way to test what products its users will be intrested in
+This app will iterate 25 times displaying 3 out of 20 products to potential customers and calculate the percentage that each item was dispayed and clicked.
+
